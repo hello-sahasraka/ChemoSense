b1f60d40499cfeb8fb300e631bd90cd0eb75c994
hello-sahasraka <sahasrakasachin@gmail.com>
2025-04-22 23:05:39 +0530

    Initializing mobile-app

--- chemosense-frontend-mobile/app/index.tsx
+++ chemosense-frontend-mobile/app/index.tsx
@@ -1,14 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Dummy from '@/components/Dummy'
 import "../global.css"
-
-// Components
-import Dummy from '../components/Dummy'
 
 const index = () => {
   return (
-    <View className='flex-1 items-center justify-center bg-yellow-200'>
-      <Text className='text-2xl font-bold text-blue-500'>Hello!</Text>
+    <View className='items-center justify-center flex-1 bg-yellow-200'>
+      <Text className='text-2xl text-blue-500'>Hello World!</Text>
       <Dummy />
     </View>
   )
