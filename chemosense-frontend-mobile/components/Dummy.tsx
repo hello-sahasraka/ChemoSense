60cf590658bb53bcc1b1a96b8d115e8e5fa91eb7
hello-sahasraka <sahasrakasachin@gmail.com>
2025-04-22 23:17:22 +0530

    fix:nativewind

--- chemosense-frontend-mobile/components/Dummy.tsx
+++ chemosense-frontend-mobile/components/Dummy.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Dummy = () => {
   return (
     <View>
-      <Text className='font-bold text-red-600'>This is a Example for Custom Components. Do not use this!</Text>
+      <Text className='font-bold text-green-600'>This is a Example for Custom Components. Do not use this!</Text>
     </View>
   )
 }
