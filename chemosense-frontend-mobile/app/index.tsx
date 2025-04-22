ffe07e30264eead729a19a642b5312fa4bcd6e67
hello-sahasraka <sahasrakasachin@gmail.com>
2025-04-22 20:58:54 +0530

    fix(tailwind): update config for NativeWind compatibility

--- chemosense-frontend-mobile/app/index.tsx
+++ chemosense-frontend-mobile/app/index.tsx
@@ -1,13 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import "../global.css"
 
 // Components
 import Dummy from '../components/Dummy'
 
 const index = () => {
   return (
-    <View className='flex-1 items-center justify-center bg-yellow-200'>
+    <View className='items-center justify-center flex-1 bg-yellow-200'>
       <Text className='text-2xl font-bold text-blue-500'>Hello!</Text>
       <Dummy />
     </View>
