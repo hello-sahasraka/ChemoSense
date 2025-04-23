27ff5943e61be9d85ccd90ff4aa26add9c30b6c0
MadaraGallage <madaragallage2002@gmail.com>
2025-04-23 22:19:29 +0530

    Patient Edit

--- chemosense-frontend/src/pages/Admin/AdminEditSinglePat.jsx
+++ chemosense-frontend/src/pages/Admin/AdminEditSinglePat.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import DocEdit from '../../components/DocEdit'
+import PatEdit from '../../components/PatEdit'
 
 const AdminEditSinglePat = () => {
   return (
     <div className='w-full h-auto relative'>
         <div className="bg-white p-6 rounded-2xl shadow-lg w-full h-[450px] absolute top-[67px]">
-          Madara, this section is for developing a single patient detail list. Do your best and be meticulous with alignments. 
-          Follow best practices and use clear, simple techniques to ensure precision. Maintain high quality throughout.
+        <PatEdit/>
         </div>
     </div>
   )
