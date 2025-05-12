ac4bb28ac6715411ab5b51e159988f5fcdea4201
Chuthuka <chuthuka097dedunu@gmail.com>
2025-05-12 12:37:53 +0530

    Fix Errors

--- chemosense-frontend-mobile/app/forgot-password/_layout.tsx
+++ chemosense-frontend-mobile/app/forgot-password/_layout.tsx
@@ -4,11 +4,10 @@ import React from 'react';
 export default function AppLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="PatLogIn" options={{ title: 'Login', headerShown: false }} />
+      <Stack.Screen name="ResetPasswordEnterNICScreen" options={{ headerShown: false }} />
       
-      <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
-      <Stack.Screen name="dashboard" options={{ headerShown: false }} />
+      <Stack.Screen name="ResetPasswordVerifyCodeScreen" options={{ headerShown: false }} />
+      <Stack.Screen name="ResetPasswordSetNewScreen" options={{ headerShown: false }} />
     </Stack>
   );
 }
